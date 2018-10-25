768276dbd0375ce5a7d71b0faecf6b2c1874b9ba
Tim Nelson <timlnelson10@gmail.com>
2018-10-25 16:59:21 -0700

    Initial Commit

---
+++ HelpMe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelpMe
+//
+//  Created by Timothy Nelson on 10/25/18.
+//  Copyright © 2018 Timothy Nelson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelpMe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelpMe
+//
+//  Created by Timothy Nelson on 10/25/18.
+//  Copyright © 2018 Timothy Nelson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
